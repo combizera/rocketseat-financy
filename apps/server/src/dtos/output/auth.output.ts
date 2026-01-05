760de0dde135c6539faa3d49cfa58e7d812b20d9
combizera <ycombi349@gmail.com>
2026-01-05 20:31:09 -0300

    feat: add InputType decorator

--- apps/server/src/dtos/output/auth.output.ts
+++ apps/server/src/dtos/output/auth.output.ts
@@ -1,16 +1,18 @@
-import { Field } from "type-graphql";
+import { Field, InputType } from "type-graphql";
 
+@InputType()
 export class LoginOutput {
   @Field(() => String)
   userId!: string;
-
+  
   @Field(() => String)
   token!: string;
-
+  
   @Field(() => String)
   refreshToken!: string;
 }
 
+@InputType()
 export class RegisterOutput {
   @Field(() => String)
   userId!: string;
