bc93e5275f3d5af0fcf74dd1c6f7a4c0da794b32
combizera <ycombi349@gmail.com>
2026-01-09 18:35:39 -0300

    feat: remove createUser mutation

--- apps/server/src/resolvers/user.resolver.ts
+++ apps/server/src/resolvers/user.resolver.ts
@@ -1,5 +1,4 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql"
-import { RegisterInput } from "@/dtos/input/auth.input"
+import { Arg, Query, Resolver } from "type-graphql"
 import { UserModel } from "@/models/user.model"
 import { UserService } from "@/services/user.service"
 
@@ -12,12 +11,5 @@ export class UserResolver {
     @Arg('id', () => String) id: string
   ): Promise<UserModel> {
     return this.userService.findUser(id)
-  }
-
-  @Mutation(() => UserModel)
-  async createUser(
-    @Arg('data', () => RegisterInput) data: RegisterInput
-  ): Promise<UserModel>  {
-    return this.userService.createUser(data)
   }
 }
--- apps/server/src/services/user.service.ts
+++ apps/server/src/services/user.service.ts
@@ -1,25 +1,6 @@
 import prisma from "@financy/db"
-import type { RegisterInput } from "@/dtos/input/auth.input"
 
 export class UserService {
-  async createUser(data: RegisterInput) {
-    const findUser = await prisma.user.findUnique({
-      where: {
-        email: data.email,
-      },
-    })
-
-    if (findUser) throw new Error("User already exists")
-
-    return prisma.user.create({
-      data: {
-        name: data.name,
-        email: data.email,
-        password: data.password,
-      },
-    })
-  }
-
   async findUser(id: string) {
     const user = await prisma.user.findUnique({
       where: { id },
